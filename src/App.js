27cc4bae58766e869e4ee9255772b7292ce126a0
NEETAMOURYA <neeta.mourya@bridgelabz.com>
2022-04-05 21:16:30 +0530

    Refector: app.js to display “Hello from BridgeLabz” using One Way Data Bindings

--- src/App.js
+++ src/App.js
@@ -2,38 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <h1> hello from bridgelabz </h1>
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
 class App extends React.Component {
 
-  constructor(){
+  constructor() {
     super()
     this.state = {
       title: 'hello from bridgelabz'
     }
   }
-  render(){
-    return(
+  render() {
+    return (
       <div>
         <h1>{this.state.title}</h1>
       </div>
